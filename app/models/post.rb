26ada851d73250682d30b53877135be4afb4166e
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-06-25 09:32:49 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,16 @@ class Post < ActiveRecord::Base
   has_many :tags, :through => :post_tags
 
   validates_presence_of :name, :content
+
+  def tags_attributes=(tag_hash)
+    if tag_hash[:name].present?
+      tag = Tag.find_or_create_by(tag_hash)
+      self.post_tags.build(tag_id: tag.id)
+      self.save
+    else
+      false
+    end
+
+  end
+
 end
